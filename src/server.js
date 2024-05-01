f311a75ac7798501e8b353480fa695caa89b405e
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-04-30 23:32:55 -0500

    Added server listening

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-require('dotenv').config()
-console.log(process.env.PORT)
+const appRoutes = require('./app/routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("Hello World");
+})
+
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}...`);
+})
